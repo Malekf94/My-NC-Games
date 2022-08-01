2403d3c7633f9bf36693cf965f2141e669e8c5b5
Malek Farhat <malek.f.94@gmail.com>
2022-08-01 15:10:50 +0100

    final test for ticket 4

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((err, req, res, next) => {
 	if (err.status && err.msg) {
 		res.status(err.status).send({ msg: err.msg });
 	} else if (err.code === "22P02") {
-		res.status(400).send({ msg: "Bad Request" });
+		res.status(400).send({ msg: "Bad Request!" });
 	}
 });
 
